17fb4b49b6aeafc5fe81708436922a25517d4ba3
Uros Pocek <uros.pocek@gmail.com>
2024-11-02 15:55:10 +0100

    feat: Added thank you page

--- bucha/components/landing-cards/video-card/VideoCard.jsx
+++ bucha/components/landing-cards/video-card/VideoCard.jsx
@@ -22,7 +22,7 @@ export default function VideoCard({ title, text, buttons, bgColor, lightBgColor,
         <div className={styles.card}>
             <div className={styles.container} style={{ background: bgColor }}>
                 <div className={`${styles.left} ${order == 1 ? styles.order1 : styles.order2}`} style={{ background: lightBgColor }}>
-                    <video autoPlay loop style={{ width: (screenWidth > 1400 ? 720 : (screenWidth > 1200 ? 691 : (screenWidth > 720 ? 720 : 360))), height: (screenWidth > 1400 ? 625 : (screenWidth > 1200 ? 599.83 : (screenWidth > 720 ? 625 : 312.5))) }} muted src={videoUrl} controls />
+                    <video src={videoUrl} autoPlay loop muted controls webkit-playsinline playsinline style={{ width: (screenWidth > 1400 ? 720 : (screenWidth > 1200 ? 691 : (screenWidth > 720 ? 720 : 360))), height: (screenWidth > 1400 ? 625 : (screenWidth > 1200 ? 599.83 : (screenWidth > 720 ? 625 : 312.5))) }} />
                 </div>
                 <div className={`${styles.right} ${order == 1 ? styles.order2 : styles.order1}`}>
                     <div>
